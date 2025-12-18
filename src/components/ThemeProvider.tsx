d44401535a862c2d11017e56184816393f9d9077
Dalrymple <ramosdalrymple@gmail.com>
2025-12-18 22:21:34 +0800

    fix: next-themes import for next 16

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-// This is the safest way to get the props in the new version
 export function ThemeProvider({ 
   children, 
   ...props 
