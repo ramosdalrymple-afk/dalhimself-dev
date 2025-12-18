50813f43dd9140bdcfec3127cd7a9550b48edc9b
Dalrymple <ramosdalrymple@gmail.com>
2025-12-18 22:19:00 +0800

    fix: robust theme provider types

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,8 +2,11 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// This is the safest way to get the props in the new version
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
